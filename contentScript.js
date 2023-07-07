d78a4939293bb12348cc059c0a4f9e467fd8f697
Ridha Chowdhury <chowdhuryridha@gmail.com>
2023-07-06 23:38:52 -0400

    OpenAI api call functionality

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,35 @@
+async function fetchOpenAIData(prompt, temperature, max_tokens) {
+    const fetchData = async () => {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer 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",
+        },
+        body: JSON.stringify({
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": prompt}],
+            "temperature": temperature,
+        }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } 
+        
+        else {
+            return null;
+        }
+    };
+
+    const data = await fetchData();
+
+    return data;
+}
+  
+
+  
 function extractCodeFromHTML(html) {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
@@ -7,7 +39,6 @@ function extractCodeFromHTML(html) {
     spanElements.forEach(span => {
         const lineText = Array.from(span.childNodes)
         .map(node => {
-            console.log(node.textContent)
             return node.textContent.replace("&nbsp;", ' ');
         })
         .join('');
@@ -16,11 +47,22 @@ function extractCodeFromHTML(html) {
 
     return code.trim();
 }
-
+  
 function scrapeCode() {
     const element = document.querySelector('.view-lines.monaco-mouse-cursor-text');
-    console.log(element)
-    console.log(element.innerHTML);
     const code = extractCodeFromHTML(element.innerHTML);
     return code;
 }
+  
+window.onload = async function() {
+    setTimeout(async () => {
+        let userCode = scrapeCode();
+        
+        let query = "Provide insights on how the user could get the code to work. Dont just tell them the answer, but give them hints on how to get there." +
+        "This is the user code: " + userCode;
+
+        let response = await fetchOpenAIData(query, 0.5, 50);
+        console.log(response);
+    }, 5000);
+};
+  
